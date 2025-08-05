f83210d63b069e4da556641111d9b67d2bea4219
andressaurbano <andressa.urbano@hoamtechnology.com>
2025-08-05 17:12:04 -0300

    improve calculation

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,12 +8,18 @@ import (
 func main() {
 	const inflationRate float64 = 2.5
 
-	var investmentAmount float64 = 1000
-	var years float64 = 10
+	var investmentAmount float64
+	var years float64
+
+	fmt.Print("Enter your investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := calculateValue(investmentAmount, inflationRate, years)
 
-	fmt.Println(futureValue)
+	fmt.Println("Result: ", futureValue)
 }
 
 func calculateValue(investmentAmount float64, inflationRate float64, years float64) float64 {
